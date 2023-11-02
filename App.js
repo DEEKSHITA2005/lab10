1e92e1adfc16bc8112ddf12e2c616cc803d7c457
DEEKSHITA2005 <108971466+DEEKSHITA2005@users.noreply.github.com>
2023-11-02 20:34:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import FetchData1 from './components/FetchData1';
+// import FetchData from './components/FetchData';
+// import NameList from './components/NameList';
+// import StudentList from './components/StudentList';
+
+function App() {
+  return (
+    <div >
+      {/* <NameList/> */}
+      {/* <StudentList/> */}
+      {/* <FetchData/> */}
+      <FetchData1/>
+    </div>
+  );
+}
+
+export default App;
